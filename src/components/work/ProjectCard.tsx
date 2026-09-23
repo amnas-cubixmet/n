f8e643618788aaab5d6c9d8d7884459fc5dad938
amnas-cubixmet <ali.cubimet@gmail.com>
2026-09-23 09:53:53 +0530

    fix: make project cards valid touch-friendly links

--- src/components/work/ProjectCard.tsx
+++ src/components/work/ProjectCard.tsx
@@ -70,11 +70,11 @@ export function ProjectCard({
         isEvenMobile ? "ml-auto lg:ml-0" : "mr-auto lg:mr-0"
       } ${gridClass}`}
     >
-      <TransitionLink href={`/services/${project.slug}`} className="block w-full">
-        <button
-          type="button"
-          className="group relative flex w-full flex-col items-start text-left cursor-pointer"
-        >
+      <TransitionLink
+        href={`/services/${project.slug}`}
+        className="group block w-full focus:outline-none focus-visible:ring-2 focus-visible:ring-[#1677FF] focus-visible:ring-offset-2 focus-visible:ring-offset-white"
+      >
+        <div className="relative flex w-full flex-col items-start text-left cursor-pointer">
           {/* CONTROLLED 29/34 PORTRAIT ASPECT RATIO WITH DIAGONAL TOP-LEFT CLIP */}
           <div
             className="project-image relative w-full aspect-[29/34] overflow-hidden"
@@ -105,7 +105,7 @@ export function ProjectCard({
               {project.category}
             </span>
           </div>
-        </button>
+        </div>
       </TransitionLink>
     </article>
   );

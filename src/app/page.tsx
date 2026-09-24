205e8e180b44a02b3158005d0de5fdd189cd4311
amnas-cubixmet <ali.cubimet@gmail.com>
2026-09-25 00:15:51 +0530

    Restore Wow statement section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import WatWeDoen from "@/components/home/WatWeDoen";
 import OurExpertise from "@/components/home/OurExpertise";
 import SelectedWork from "@/components/home/SelectedWork";
 import DeliverablesSection from "@/components/home/DeliverablesSection";
+import StatementSection from "@/components/home/StatementSection";
 import OurVision from "@/components/home/OurVision";
 import OurUSPs from "@/components/home/OurUSPs";
 import FoundedOnAVision from "@/components/home/FoundedOnAVision";
@@ -64,6 +65,9 @@ export default function Home() {
 
         {/* DELIVERABLES */}
         <DeliverablesSection />
+
+        {/* WOOOW STATEMENT */}
+        <StatementSection />
 
         {/* OUR VISION SECTION */}
         <OurVision />
